254719862e2fe615ab4bd8cbb7c7097d9b2c8495
jeeban <jeebanjyoti193@gmail.com>
2023-07-11 12:33:24 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace example_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 10; i++)
+            {
+                if (i == 6)
+                {
+                    goto end;
+                }
+                Console.WriteLine("i value:{0}", i);
+
+            }
+
+        end: Console.WriteLine("The end");
+        }
+    }
+}
